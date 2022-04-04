9beb340ee1971a782fdda1a43b62ae8f4a82751d
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-04-04 11:22:10 +0200

    Teszt javítás

--- src/test/java/TestNumberOfValleys.java
+++ src/test/java/TestNumberOfValleys.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class TestNumberOfPeaks {
+public class TestNumberOfValleys {
 
    @Test
    public void testNoArray() {
